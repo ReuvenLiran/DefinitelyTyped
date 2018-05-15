b3eaa066f5343006422576c82b32852e45cc89a1
Tim Branyen <tbranyen@netflix.com>
2018-05-15 16:30:49 -0700

    Add Swagger Dist Typings

---
+++ types/swagger-ui-dist/index.d.ts
@@ -0,0 +1,10 @@
+// Type definitions for swagger-ui-dist 3.14.2
+// Project: https://github.com/swagger-api/swagger-ui
+// Definitions by: Tim Branyen <https://github.com/tbranyen>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.8
+
+export function getAbsolutePath(): string;
+export function absolutePath(): string;
+export function SwaggerUIBundle(): void
+export function SwaggerUIStandalonePreset(): void
